2d586b01a125aa003cb69a3425f8920b1a325cc1
roman <romansheikh170@gmail.com>
2021-09-20 00:59:02 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Countries from './Components/Countries/Countries';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Countries></Countries>
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>see all countries all over the world</h2>
+        </div>
+    );
+};
+
+export default Header;
