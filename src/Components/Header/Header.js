cfcd9dfd0179471c5205e890af104bac5ad90030
roman <romansheikh170@gmail.com>
2021-09-20 01:03:52 +0600

    --- Auto-Git Commit ---

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="header">
+        <div className="header bg">
             <h2>see all countries all over the world</h2>
         </div>
     );
