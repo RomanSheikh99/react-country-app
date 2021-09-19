6505ccbd9af24812e7eea1afeef4a590b6d92559
roman <romansheikh170@gmail.com>
2021-09-20 01:04:47 +0600

    add bootstrap

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="header bg">
+        <div className="header bg-info text-white">
             <h2>see all countries all over the world</h2>
         </div>
     );
